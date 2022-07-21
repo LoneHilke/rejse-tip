0e811d78d176c74bbf74980fff20f1cf62761db3
Lone Hilke <lonehilke@gmail.com>
2022-07-21 22:07:01 +0200

    tilretning
    skal til tilretning og superuser
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- tip/rejse/admin.py
+++ tip/rejse/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Profile, Rejse, Comment
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Rejse)
+admin.site.register(Comment)
--- tip/tip/urls.py
+++ tip/tip/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('rejse.urls')),
+    path('', include('rejse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
